6cf7fa208c247b916c1143d9601b0265b1d274ad
Giovane Machado <giovanemachado3@gmail.com>
2024-10-05 12:23:38 -0300

    configuring gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,15 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter(),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+		}
 	}
 };
 
 export default config;
-
