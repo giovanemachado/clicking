e19d16528009c323e6c4e19c7ab89983e512ae89
Giovane Machado <giovanemachado3@gmail.com>
2024-10-05 12:30:11 -0300

    add fallback

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
 		}
